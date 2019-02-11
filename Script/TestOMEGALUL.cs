c4165295d0ecf70d03e4f470904ebb75ef28dffc
Baris Buba <Skullyurdb@gmail.com>
2019-02-11 11:03:28 +0100

    d

--- Script/TestOMEGALUL.cs
+++ Script/TestOMEGALUL.cs
@@ -7,12 +7,12 @@ public class TestOMEGALUL : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //wanneer ik mais eet en poep, poep ik dan een maiskolf?
-    }
+        //卐卐卐卐卐卐卐卐卐卐
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
